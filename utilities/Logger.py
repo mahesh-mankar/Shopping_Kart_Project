c8c1877f9e7e71318277a79f56e18748892eb574
mahesh-mankar <mmankar2@gmail.com>
2023-10-11 21:17:59 +0530

    Set logger

--- utilities/Logger.py
+++ utilities/Logger.py
@@ -1,12 +1,14 @@
+import inspect
 import logging
 
 
 class LogGen:
     @staticmethod
     def loggen():
-        logger = logging.getLogger()
+        classname = inspect.stack()[1][3]
+        logger = logging.getLogger(classname)
         file = logging.FileHandler("C:\\Users\\LENOVO\\PycharmProjects\\pythonProject\\Shopping_Kart\\Logs\\logfile.log")
-        formats = logging.Formatter("%(asctime)s : %(levelname)s : %(name)s : %(message)s")
+        formats = logging.Formatter("%(asctime)s : %(levelname)s : %(name)s : %(funcName)s : %(message)s")
         file.setFormatter(formats)
         logger.addHandler(file)
         logger.setLevel(logging.INFO)
