e20987e7997bd48a6713e17a56840d7c8a03f0f8
mahesh-mankar <mmankar2@gmail.com>
2023-10-11 17:36:05 +0530

    Capture Screenshots

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -8,8 +8,10 @@ class Test_URL_Login:
     def test_URL_Validation(self, setup):
         self.driver = setup
         if self.driver.title == "Swag Labs":
+            self.driver.save_screenshot("C:\\Users\\LENOVO\\PycharmProjects\\pythonProject\\Shopping_Kart\\Screenshots\\test_URL_Validation_Pass.png")
             assert True
         else:
+            self.driver.save_screenshot("C:\\Users\\LENOVO\\PycharmProjects\\pythonProject\\Shopping_Kart\\Screenshots\\test_URL_Validation_Fail.png")
             assert False
 
     def test_login_002(self, setup):
@@ -20,8 +22,10 @@ class Test_URL_Login:
         self.lp.click_login()
         time.sleep(5)
         if self.lp.login_status() == True:
+            self.driver.save_screenshot("C:\\Users\\LENOVO\\PycharmProjects\\pythonProject\\Shopping_Kart\\Screenshots\\test_login_002_Pass.png")
             self.lp.click_menu_button()
             self.lp.click_logout_button()
             assert True
         else:
+            self.driver.save_screenshot("C:\\Users\\LENOVO\\PycharmProjects\\pythonProject\\Shopping_Kart\\Screenshots\\test_login_002_Fail.png")
             assert False
