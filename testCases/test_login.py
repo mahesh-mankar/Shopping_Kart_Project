eda3abfd88e698ef0e2d78c2977884b2eba2168e
mahesh-mankar <mmankar2@gmail.com>
2023-10-11 16:33:52 +0530

    common method created using fixture

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -1,40 +1,35 @@
 import time
 
-from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 
 class Test_URL_Login:
 
-    def test_URL_Validation(self):
-        driver = webdriver.Chrome()
-        driver.get('https://www.saucedemo.com/')
-        if driver.title == "Swag Labs":
+    def test_URL_Validation(self, setup):
+        self.driver = setup
+        if self.driver.title == "Swag Labs":
             assert True
         else:
             assert False
 
-    def test_login_002(self):
-        driver = webdriver.Chrome()
-        driver.get('https://www.saucedemo.com/')
-        driver.find_element(By.ID, 'user-name').send_keys("standard_user")
-        driver.find_element(By.ID, 'password').send_keys("secret_sauce")
-        driver.find_element(By.ID, 'login-button').click()
+    def test_login_002(self, setup):
+        self.driver = setup
+        self.driver.find_element(By.ID, 'user-name').send_keys("standard_user")
+        self.driver.find_element(By.ID, 'password').send_keys("secret_sauce")
+        self.driver.find_element(By.ID, 'login-button').click()
         try:
-            driver.find_element(By.XPATH, '//span[@class="title"]')
+            self.driver.find_element(By.XPATH, '//span[@class="title"]')
             login = True
         except NoSuchElementException:
             login = False
             pass
         time.sleep(5)
         if login == True:
-            driver.find_element(By.ID, 'react-burger-menu-btn').click()
-            driver.find_element(By.ID, 'logout_sidebar_link').click()
+            self.driver.find_element(By.ID, 'react-burger-menu-btn').click()
+            self.driver.find_element(By.ID, 'logout_sidebar_link').click()
             assert True
         else:
             assert False
-
-
 
 
